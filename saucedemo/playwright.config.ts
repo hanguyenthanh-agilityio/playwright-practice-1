d6ec696f7d303e6e4cb4f165c82afbab69b96249
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2026-03-10 15:25:28 +0700

    fix: update config

--- saucedemo/playwright.config.ts
+++ saucedemo/playwright.config.ts
@@ -1,84 +1,83 @@
 import { defineConfig, devices } from '@playwright/test'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+const isCI = !!process.env.CI
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
+
   fullyParallel: true,
 
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  forbidOnly: isCI,
+
+  retries: isCI ? 2 : 0,
+
+  workers: isCI ? 1 : undefined,
+
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+
   use: {
     baseURL: 'https://www.saucedemo.com/',
+
+    headless: isCI ? true : false,
+
     trace: 'on-first-retry',
-    headless: false,
-    launchOptions: {
-      slowMo: 1000,
-    },
+
     screenshot: 'only-on-failure',
+
     video: 'retain-on-failure',
+
+    launchOptions: {
+      slowMo: isCI ? 0 : 300,
+    },
   },
 
-  /* Configure projects for major browsers */
-  projects: [
-    // Setup project (login)
-    {
-      name: 'setup',
-      testMatch: /.*\.setup\.ts/,
-    },
+  projects: isCI
+    ? [
+        {
+          name: 'setup',
+          testMatch: /.*\.setup\.ts/,
+        },
 
-    // Chromium tests
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        storageState: 'playwright/.auth/user.json',
-      },
-      dependencies: ['setup'],
-    },
+        {
+          name: 'chromium',
+          use: {
+            ...devices['Desktop Chrome'],
+            storageState: 'playwright/.auth/user.json',
+          },
+          dependencies: ['setup'],
+        },
+      ]
+    : [
+        {
+          name: 'setup',
+          testMatch: /.*\.setup\.ts/,
+        },
 
-    // Firefox tests
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-        storageState: 'playwright/.auth/user.json',
-      },
-      dependencies: ['setup'],
-    },
+        {
+          name: 'chromium',
+          use: {
+            ...devices['Desktop Chrome'],
+            storageState: 'playwright/.auth/user.json',
+          },
+          dependencies: ['setup'],
+        },
 
-    // Webkit tests
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-        storageState: 'playwright/.auth/user.json',
-      },
-      dependencies: ['setup'],
-    },
-  ],
+        // {
+        //   name: 'firefox',
+        //   use: {
+        //     ...devices['Desktop Firefox'],
+        //     storageState: 'playwright/.auth/user.json',
+        //   },
+        //   dependencies: ['setup'],
+        // },
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+        {
+          name: 'webkit',
+          use: {
+            ...devices['Desktop Safari'],
+            storageState: 'playwright/.auth/user.json',
+          },
+          dependencies: ['setup'],
+        },
+      ],
 })
